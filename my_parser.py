d17842907e84ca49b9c99ab368a03b3a3f9839a6
AAPTRV <alekseyandreevichpetrov@mail.ru>
2022-05-29 19:00:54 +0300

    Parsing of objects Project implemented

--- my_parser.py
+++ my_parser.py
@@ -6,11 +6,11 @@ url = "https://daomaker.com/"
 # for item in list_of_projects:
 #     print(util_project.transformer.get_url_from_project_name(item, url))
 
-list_of_url_projects = util_project.projects_names_parser.get_url_list_of_projects(url)
+list_of_url_projects = util_project.projects_names_parser.get_projects_as_objects(url)
 
-for item in list_of_url_projects:
-    print(item)
-
-
+for project in list_of_url_projects:
+    print(project.get_name())
+    print(project.get_url())
+    print("\n")
 
 
--- util_project/projects_names_parser.py
+++ util_project/projects_names_parser.py
@@ -3,6 +3,56 @@ import time
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
+from classes.Project import Project
+
+
+def get_projects_as_objects(url):
+    result_list = []
+    driver = webdriver.Chrome(executable_path="/Users/eaxes/DA Projects/CMC/chromedriver/chromedriver")
+
+    try:
+        driver.get(url=url)
+        time.sleep(20)
+    #     # TODO time.sleep not a constant but wait until page is fully opened
+    #
+    #     button = driver.find_elements_by_xpath('//a[normalize-space()="Load More"]')
+    #     while len(button) > 0:
+    #         ActionChains(driver).move_to_element(button[0]).click(button[0]).perform()
+    #         time.sleep(3)
+    #         button = driver.find_elements_by_xpath('//a[normalize-space()="Load More"]')
+
+        companies_list = driver.find_elements_by_class_name("company_single")
+
+        for company in companies_list:
+
+            name = company.find_element_by_class_name("comp_details_header") \
+                .find_element(By.CSS_SELECTOR, "h4") \
+                .get_attribute("innerHTML") \
+                .replace("<span>", "") \
+                .replace("</span>", "") \
+                .strip()
+
+            url = company.find_element_by_class_name("btn.wide.btn_square.see_details") \
+                .get_attribute("href")
+
+            result_list.append(Project(name=name, url=url))
+
+        print(len(companies_list))
+
+
+
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        driver.close()
+        driver.quit()
+
+
+
+    return result_list
+
+
 def get_url_list_of_projects(url):
     result_list = []
 
@@ -49,11 +99,12 @@ def get_list_of_projects(url):
 
         some_data = driver.find_elements_by_class_name("comp_details_header")
         for item in some_data:
-            result_list.append(item.find_element(By.CSS_SELECTOR, "h4") \
+            name = item.find_element(By.CSS_SELECTOR, "h4") \
                                .get_attribute("innerHTML") \
                                .replace("<span>", "") \
                                .replace("</span>", "") \
-                               .strip())
+                               .strip()
+            result_list.append(name)
 
     except Exception as ex:
         print(ex)
