1a0906a80dac4db3064c5a133551bed055fe5497
AAPTRV <alekseyandreevichpetrov@mail.ru>
2022-06-02 23:26:00 +0300

    Time and value extracted from data (for token chart prices)

--- data/my_data.py
+++ data/my_data.py
@@ -2,11 +2,18 @@ import pandas as pd
 import numpy as np
 import parsing_scripts.scripts.daomaker as dm
 import parsing_scripts.parsing_tools.coingecko.coingecko_listing as cg
+from datetime import datetime
 
 df = pd.read_csv('/Users/eaxes/DA Projects/CMC/data/collected_data/sample123.csv')
 print(df.head())
 
 id = df.iloc[0]['coingecko_numerical_id']
 
-print(cg.get_coin_chart(id))
+dict = cg.get_coin_chart(id)
+for item in dict['stats']:
+    item_date = datetime.utcfromtimestamp(item[0]/1000).strftime('%Y-%m-%d %H:%M')
+    print(f"date:{item_date}")
+    print(f"value:{item[1]}")
+    print('***')
+
 
